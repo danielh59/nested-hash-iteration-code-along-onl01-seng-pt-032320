369c590754fa295db93a61b53c755f187a474eb0
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-04-05 23:28:05 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,7 +16,11 @@ require 'pry'
   }
   
 def remove_strawberry(contacts)
-contacts.each do |name, value|
-
-end
-end
+  mr_f = contacts["Freddy Mercury"]
+  mr_f.each do |attribute, value|
+    if attribute == :favorite_icecream_flavors
+      value.delete_if{ |ice_cream| ice_cream == "strawberry" }
+    end
+  end
+  contacts
+end 
