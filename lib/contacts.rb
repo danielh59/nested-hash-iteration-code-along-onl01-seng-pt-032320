25986afb04e96cc2812064b823a856d0ba435f21
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-04-05 23:22:25 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,7 +19,7 @@ def remove_strawberry(contacts)
 abc = contacts["Freddy Mercury"][:favorite_ice_cream_flavors]
 if abc.each do |x|
   x == "strawberry"
-  favorite_ice_cream_flavors.splice("strawberry")
+  x.splice("strawberry")
 end
 end
 end
